9031da62eedc95d4e01b92270d2092d4d9b05c9f
Osama Mahmood <osama.mahmood40@gmail.com>
2016-12-19 20:51:53 +0500

    Fully working Foodiespk app

--- foodiespk/settings.py
+++ foodiespk/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+GEOPOSITION_GOOGLE_MAPS_API_KEY = 'AIzaSyDO8HArWZPROayaLrDq6EDMn0yqwFE-0zc'
 
 # Application definition
 
@@ -38,6 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'widget_tweaks',
+    'sitegate',
+    'bootstrap3',
+    'geoposition',
+    'bootstrap_pagination',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request"
 ]
 
 MIDDLEWARE = [
